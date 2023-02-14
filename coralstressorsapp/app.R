6275383fa8bd76f9bd6271349f153616aadcdc66
HeatherNL18 <luedkeheather@gmail.com>
2023-02-14 15:29:45 -0800

    First graph added in

--- coralstressorsapp/app.R
+++ coralstressorsapp/app.R
@@ -2,19 +2,23 @@ library(shiny)
 library(tidyverse)
 library(bslib) #themes for shinyapp
 
+
+
 #SETUP THE THEME - copied from lab last week we can change
 my_theme <- bs_theme(
-  bg = "rgba(170, 208, 243)", #copy and pasted from the theme preview
-  fg = "purple",
-  primary = "yellow",
-  base_font = font_google("Times")
+  bg = "rgba(170, 208, 243)", #copy and pasted from the theme preview, background
+  fg = "blue", #
+  primary = "black",
+  base_font = font_google("Arial")
 )
 
 #bs_theme_preview() lets you use a style sheet to make it pretty
 #another way to do this during lab week 3's video, making a css file
 
-
-
+#datasets?
+top10_species <- corals_info %>%
+  filter(species == c("acanthastrea brevis", "acanthastrea echinata", "acanthastrea hemprichii")) %>%  #change these later depending on what species we want
+  return(top10_species)
 
 
 
@@ -25,7 +29,7 @@ my_theme <- bs_theme(
 ######USER INTERFACE########
 ui <- fluidPage(theme = my_theme,
                 navbarPage(
-                  "App Title Here",
+                  "Coral Vulnerability to Stressors",
 
 
 
@@ -33,68 +37,79 @@ ui <- fluidPage(theme = my_theme,
 
 
                   #MAP ONE - ELERI
-                  tabPanel("Thing 1",  #tabs up at the top we can select between
+               #   tabPanel("Thing 1",  #tabs up at the top we can select between
+                #           sidebarLayout( #creates a page that has a sidebar on one side that we can put widgets/explanations on one side, and then a larger panel on the right for graph/map
+                 #            sidebarPanel("Widgets",
+                  #                        checkboxGroupInput(
+                   #                         inputId = "pick_species", label = "Choose Species:",
+                       #                     choices = unique(dataset$columnn_name)
+                        #                  )
+                         #    ), #end sidebarPanel
+                          #   mainPanel("Output",
+                           #            plotOutput("plot_1")) #call your graph or thing from below here, this line of code comes from what you called your plot in output$plot below in the server
+                          # ) #end sidebar layout
+             #     ), #end tabPanel("Thing 1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#GRAPHS - HEATHER
+                  tabPanel("Species Vulnerability Graphs",  #tabs up at the top we can select between
                            sidebarLayout( #creates a page that has a sidebar on one side that we can put widgets/explanations on one side, and then a larger panel on the right for graph/map
                              sidebarPanel("Widgets",
-                                          checkboxGroupInput(
-                                            inputId = "pick_species", label = "Choose Species:",
-                                            choices = unique(dataset$columnn_name)
+                                         selectInput(
+                                            inputId = "pick_species", label = "Choose Species:",  #what goes in the input id?
+                                            choices = unique(top10_species$species) #gives the options for the checkboxes
                                           )
                              ), #end sidebarPanel
                              mainPanel("Output",
-                                       plotOutput("plot_1")) #call your graph or thing from below here, this line of code comes from what you called your plot in output$plot below in the server
-                           ) #end sidebar layout
-                  ), #end tabPanel("Thing 1")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                  #GRAPHS - HEATHER
-                  tabPanel("Thing 2",  #tabs up at the top we can select between
-                           sidebarLayout( #creates a page that has a sidebar on one side that we can put widgets/explanations on one side, and then a larger panel on the right for graph/map
-                             sidebarPanel("Widgets",
-                                          checkboxGroupInput(   #check multiple items and change output (filter)
-                                            inputId = "pick_species", label = "Choose Species:",
-                                            choices = unique(dataset$columnn_name) #gives the options for the checkboxes
-                                          )
-                             ), #end sidebarPanel
-                             mainPanel("Output",
-                                       plotOutput("plot_1")) #call your graph or thing from below here, this line of code comes from what you called your plot in output$plot below in the server
+                                       plotOutput("species_graph")) #call your graph or thing from below here, this line of code comes from what you called your plot in output$plot below in the server
                            ) #end sidebar layout
                   ), #end tabPanel("Thing 2")
+
+                 # tabPanel("Stressor Graphs",
+                          # sidebarLayout(
+                          #   sidebarPanel("Widgets",
+                                    #      selectInput(
+                                        #    inputID = "pick_stressor", label = "Choose Stressor:",
+                                         #   choices = unique(top10_species$stressor)
+                                     #     )
+                          #    ) #end sidebar panel
+                        #   ) #end sidebar layout
+                      #  ), #end tabPanel
 
 
 
@@ -133,18 +148,18 @@ ui <- fluidPage(theme = my_theme,
 
 
                   #MAP TWO - MELISSA
-                  tabPanel("Thing 3",  #tabs up at the top we can select between
-                           sidebarLayout( #creates a page that has a sidebar on one side that we can put widgets/explanations on one side, and then a larger panel on the right for graph/map
-                             sidebarPanel("Widgets",
-                                          checkboxGroupInput(
-                                            inputId = "pick_species", label = "Choose Species:",
-                                            choices = unique(dataset$columnn_name)
-                                          )
-                             ), #end sidebarPanel
-                             mainPanel("Output",
-                                       plotOutput("plot_1")) #call your graph or thing from below here, this line of code comes from what you called your plot in output$plot below in the server
-                           ) #end sidebar layout
-                  ), #end tabPanel("Thing 3")
+             #     tabPanel("Thing 4",  #tabs up at the top we can select between
+                       #    sidebarLayout( #creates a page that has a sidebar on one side that we can put widgets/explanations on one side, and then a larger panel on the right for graph/map
+                         #    sidebarPanel("Widgets",
+                                     #     checkboxGroupInput(
+                                      #      inputId = "pick_species", label = "Choose Species:",
+                                        #    choices = unique(dataset$columnn_name)
+                                    #      )
+                        #     ), #end sidebarPanel
+                         #    mainPanel("Output",
+                        #               plotOutput("plot_1")) #call your graph or thing from below here, this line of code comes from what you called your plot in output$plot below in the server
+                         #  ) #end sidebar layout
+                #  ), #end tabPanel("Thing 4")
 
 
 
@@ -183,7 +198,7 @@ ui <- fluidPage(theme = my_theme,
 
 
                   #BACKGROUND INFO - HEATHER
-                  tabPanel("Thing 4")
+                  tabPanel("Thing 5")
 
 
 
@@ -197,7 +212,7 @@ ui <- fluidPage(theme = my_theme,
 
 
 
-                  #LEAVE THIS HERE TO END USER INTERFACE
+#LEAVE THIS HERE TO END USER INTERFACE
                 ) #end navbarPage()
 ) #end fluidPage(theme = my_theme)
 
@@ -208,13 +223,13 @@ server <- function(input, output) {
 
 
   # MAP ONE REACTIVE - ELERI
-  sw_reactive <- reactive((
-    newdataframe <- data %>%
-      filter(data_column %>% input$pickspecies) #from above
-    return(newdataframe)
-  ))
+  #sw_reactive <- reactive((
+    #data %>%
+      #filter(data_column %>% input$pick_species)  #from above
+    #return(newdataframe)
+  #))
 
-  output$plot_name <- #graph or map function like in R markdown here
+  #output$plot_name <- #graph or map function like in R markdown here
 
     #now we need to tell user interface where to put the plot we created. go back up to UI and show where you want it to go
 
@@ -258,16 +273,20 @@ server <- function(input, output) {
 
 
   # GRAPHS REACTIVE - HEATHER
-  graph_1 <- reactive((
-    newdataframe2 <- data %>%
-      filter(data_column %>% input$pickspecies) #what you specified above
-    return(newdataframe2)
+  graph_byspecies <- reactive((
+    top10_species %>%
+     filter(species == input$pick_species) #change these later depending on what species we want
   ))
 
-  output$plot_1 <- renderPlot(
-    ggplot(data = graph_1(), aes( x = , y = )) + #data is from the reactive above
-      geom_bar(aes(color = species))
-  )
+  output$species_graph <- renderPlot(
+    ggplot(data = graph_byspecies(), aes(x = stressor, y = vuln)) +
+      geom_bar(aes(color = species)))
+
+ # output$stressor_graph <- renderPlot(
+  #  ggplot(data = graph_byspecies(), aes(x = species, y = vuln) +
+   #   geom_bar(aes(color = stressor))
+ # )
+#  )
 
   #now we need to tell user interface where to put the plot we created. go back up to UI and show where you want it to go
 
@@ -309,13 +328,11 @@ server <- function(input, output) {
 
   # MAP TWO REACTIVE - MELISSA
 
-  sw_reactive_2 <- reactive((
-    newdataframe_2 <- data %>%
-      filter(data_column %>% input$pickspecies) #from above
-    return(newdataframe_2)
-  ))
+  #sw_reactive_2 <- reactive((
 
-  output$plot_name <- #graph or map function like in R markdown here
+ # ))
+
+  #output$plot_name <- #graph or map function like in R markdown here
 
     #now we need to tell user interface where to put the plot we created. go back up to UI and show where you want it to go
 
